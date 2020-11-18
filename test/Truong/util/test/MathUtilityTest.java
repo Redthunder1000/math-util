3323cb238bad0354acdbe3381889ade6c7f98bfc
Redthunder1000 <nguyenquytruong399@gmail.com>
2020-11-18 10:05:38 +0700

    push to see GREEN, RED

--- test/Truong/util/test/MathUtilityTest.java
+++ test/Truong/util/test/MathUtilityTest.java
@@ -36,6 +36,6 @@ public class MathUtilityTest {
         assertEquals(120, getFactorial(5));
         assertEquals(24, getFactorial(4));
         assertEquals(720, getFactorial(6));
-        assertEquals(1, getFactorial(0));
+        assertEquals(0, getFactorial(0));
     }
 }
