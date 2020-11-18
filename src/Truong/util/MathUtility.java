dcb192b1fee0ed0454e276d02b9b0a85fde86324
Redthunder1000 <nguyenquytruong399@gmail.com>
2020-11-18 10:26:23 +0700

    Add recursion and still see the GREEN again

--- src/Truong/util/MathUtility.java
+++ src/Truong/util/MathUtility.java
@@ -20,17 +20,27 @@ public class MathUtility {
     //n! = 1*2*3....*n
     // 0<= n <=20 , 0! = 1! = 1   (21! kiểu long đéo chơi đc)
     //âm giai thừa vô nghĩa
+//    public static long getFactorial(int n){
+//        if (n<0 || n>20)
+//            throw new IllegalArgumentException("n must be between 0..20");
+//        if (n==0 || n==1)
+//            return 1; // đặc biệt
+//        //k cần viết else ở đây
+//        //mặc nhiên CPU đến đây => n = 2..20
+//        long result = 1;
+//        for(int i=2;i<=n;i++){
+//            result *= i;
+//        }
+//        return result;
+//    }
+//  viet bang de quy
     public static long getFactorial(int n){
-        if (n<0 || n>20)
+        if (n<0 || n>20) 
             throw new IllegalArgumentException("n must be between 0..20");
-        if (n==0 || n==1)
-            return 1; // đặc biệt
-        //k cần viết else ở đây
-        //mặc nhiên CPU đến đây => n = 2..20
-        long result = 1;
-        for(int i=2;i<=n;i++){
-            result *= i;
-        }
-        return result;
+        if (n==0 || n ==1) 
+            return 1;
+        return n * getFactorial(n-1); // đệ quy
     }
+    //5! = 5 * 4!; 4! = 4 * 3!; 3! = 3 * 2!; 2! = 2 * 1!; 1! = 1
+    // => n! = n * (n-1)!
 }
